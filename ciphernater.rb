e89a57ce9c8b868a1ee64dd46a5aaad5431ec641
smysak <98991422+smysak@users.noreply.github.com>
2026-06-02 15:19:39 +0800

    make letters wrap to the start of the alphabet

--- ciphernater.rb
+++ ciphernater.rb
@@ -1,18 +1,32 @@
 def get_string
     puts "Enter the message to be enciphered:"
-    string = gets.chomp
+    string = gets.strip
 end
 
 def get_shift
     puts "Enter the desired shift:"
-    shift_amount = gets.chomp.to_i
+    shift_amount = gets.strip
+    if shift_amount.match?(/\A-?[0-9]+\z/)
+        shift_amount = shift_amount.to_i % 26
+    else
+        puts "Enter a number only, please."
+        exit(1)
+    end
+    shift_amount
 end
 
+
 def ciphernate(string, shift_amount)
-    string.strip.each_char do |char|
+    string.each_char do |char|
         if char.match?(/\A[A-Za-z]+\z/)
-            print (char.ord + shift_amount).chr
-        else print char
+            shifted_char = ((char.downcase.ord + shift_amount - 19) % 26 + 97).chr
+            if char == char.upcase
+                print shifted_char.upcase
+            else
+                print shifted_char
+            end 
+        else
+            print char
         end
     end
 end
