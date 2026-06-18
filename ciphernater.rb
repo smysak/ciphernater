2af36bc4c45a3271243640815244f20023e71c96
smysak <98991422+smysak@users.noreply.github.com>
2026-06-18 13:26:25 +0800

    employ rubocop via bundler implement some of rubocop's suggestions

--- ciphernater.rb
+++ ciphernater.rb
@@ -1,37 +1,38 @@
+# frozen_string_literal: true
+
 def get_string
-    puts "Enter the message to be enciphered:"
-    string = gets.strip
+  puts "Enter the message to be enciphered:"
+  gets.strip
 end
 
 def get_shift
-    puts ""
-    puts "Enter the desired shift:"
-    shift_amount = gets.strip
-    if shift_amount.match?(/\A-?[0-9]+\z/)
-        shift_amount = shift_amount.to_i % 26
-    else
-        puts "Enter a number only, please."
-        exit(1)
-    end
-    shift_amount
+  puts ""
+  puts "Enter the desired shift:"
+  shift_amount = gets.strip
+  if shift_amount.match?(/\A-?[0-9]+\z/)
+    shift_amount = shift_amount.to_i % 26
+  else
+    puts "Enter a number only, please."
+    exit(1)
+  end
+  shift_amount
 end
 
-
 def ciphernate(string, shift_amount)
-    puts ""
-    string.each_char do |char|
-        if char.match?(/\A[A-Za-z]+\z/)
-            shifted_char = ((char.downcase.ord + shift_amount - 19) % 26 + 97).chr
-            if char == char.upcase
-                print shifted_char.upcase
-            else
-                print shifted_char
-            end 
-        else
-            print char
-        end
+  puts ""
+  string.each_char do |char|
+    if char.match?(/\A[A-Za-z]+\z/)
+      shifted_char = ((char.downcase.ord + shift_amount - 19) % 26 + 97).chr
+      if char == char.upcase
+        print shifted_char.upcase
+      else
+        print shifted_char
+      end
+    else
+      print char
     end
-    puts ""
+  end
+  puts ""
 end
 
 ciphernate(get_string, get_shift)
--- ciphernater_file.rb
+++ ciphernater_file.rb
@@ -1,43 +1,45 @@
+# frozen_string_literal: true
+
 def get_string
-    puts "Enter the message to be enciphered:"
-    puts "(When finished, enter Ctrl+D on a new line. Use Ctrl+Z on Windows.)"
-    string = ARGF.read
+  puts "Enter the message to be enciphered:"
+  puts "(When finished, enter Ctrl+D on a new line. Use Ctrl+Z on Windows.)"
+  ARGF.read
 end
 
 def get_shift
-    puts ""
-    puts "Enter the desired shift:"
-    shift_amount = gets.strip
-    if shift_amount.match?(/\A-?[0-9]+\z/)
-        shift_amount = shift_amount.to_i % 26
-    else
-        puts "Enter a number only, please."
-        exit(1)
-    end
-    shift_amount
+  puts ""
+  puts "Enter the desired shift:"
+  shift_amount = gets.strip
+  if shift_amount.match?(/\A-?[0-9]+\z/)
+    shift_amount = shift_amount.to_i % 26
+  else
+    puts "Enter a number only, please."
+    exit(1)
+  end
+  shift_amount
 end
 
-
 def ciphernate(string, shift_amount)
-    puts ""
-    output = []
+  puts ""
+  output = []
 
-    string.each_char do |char|
-        if char.match?(/\A[A-Za-z]+\z/)
-            shifted_char = ((char.downcase.ord + shift_amount - 19) % 26 + 97).chr
-            if char == char.upcase
-                output << shifted_char.upcase
-            else
-                output << shifted_char
-            end 
-        else
-            output << char
-        end
+  string.each_char do |char|
+    if char.match?(/\A[A-Za-z]+\z/)
+      shifted_char = ((char.downcase.ord + shift_amount - 19) % 26 + 97).chr
+      output <<
+      if char == char.upcase
+        shifted_char.upcase
+      else
+        shifted_char
+      end
+    else
+      output << char
     end
-    puts ""
-    encrypted_message = output.join
-    File.write("enciphernated_message.txt", encrypted_message)
-    puts "Your message has been saved to 'enciphernated_message.txt'."
+  end
+  puts ""
+  encrypted_message = output.join
+  File.write("enciphernated_message.txt", encrypted_message)
+  puts "Your message has been saved to 'enciphernated_message.txt'."
 end
 
 ciphernate(get_string, get_shift)
